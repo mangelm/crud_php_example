6d463aeda4feb64aa787acf668372fde12f593fd
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-10-04 10:03:30 +0200

    base de datos liberia ok

--- bbdd/libreria.sql
+++ bbdd/libreria.sql
@@ -1,11 +1,18 @@
 CREATE DATABASE libreria;
 
 USE libreria;
-CREATE TABLE libro (
+CREATE TABLE libros (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     autor VARCHAR(255) NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
-    editorial VARCHAR(255) NOT NULL,
+    stock INT NOT NULL,
+    editorial VARCHAR(255) NOT NULL
 );
+
+
+INSERT INTO libros (nombre, autor, precio, stock, editorial) VALUES 
+('Cien años de soledad', 'Gabriel García Márquez', 9.99, 120, 'Editorial Sudamericana'),
+('El nombre del viento', 'Patrick Rothfuss', 14.99, 80, 'Plaza & Janés'),
+('La sombra del viento', 'Carlos Ruiz Zafón', 12.49, 100, 'Editorial Planeta');
