206619adf5fb90a521553355cf2f7d290d83e751
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-10-04 09:20:24 +0200

    tabla libros

--- bbdd/libreria.sql
+++ bbdd/libreria.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE libreria;
+
+USE libreria;
+CREATE TABLE libro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    autor VARCHAR(255) NOT NULL,
+);
